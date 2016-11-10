42e61d205608a6bfecdce6fc04673c7602bea714
freshCodeOfXiyou <18829291427@163.com>
2016-11-10 19:59:10 +0800

    javascript write

--- main.js
+++ main.js
@@ -5,14 +5,19 @@
 	cav.height=600
 	var ctx=cav.getContext('2d')
 	//define variable
-	const RADIUS=12
-	const INTERVAL=27
+	const RADIUS=13.5
+	const INTERVAL=30
 	//cav location
 	var cavTop=parseInt(cav.getBoundingClientRect().top)
 	var cavLeft=parseInt(cav.getBoundingClientRect().left)
+	//define pieces
+	var whitePieces=[]
+	var blackPieces=[]
+	//define init color
+	color='black'
 	// var cavTop=cav.getBoundingClientRect().top
 	// var cavLeft=cav.getBoundingClientRect().left
-	console.log(cavTop,cavLeft)
+	// console.log(cavTop,cavLeft)
 	/*
 	*draw the board
 	*/
@@ -37,46 +42,141 @@
         ctx.lineWidth=1
         ctx.beginPath()
 		//draw horizontal line
-		for(var i=0;i<21;i++){
-			var begin=INTERVAL+i*INTERVAL
+		for(var i=0;i<19;i++){
+			var begin=28+i*INTERVAL
 			ctx.moveTo(25,begin)
 			ctx.lineTo(570,begin)
 		}
-		for(i=0;i<21;i++){
-			begin=INTERVAL+i*INTERVAL
+		for(i=0;i<19;i++){
+			begin=29+i*INTERVAL
 			ctx.moveTo(begin,25)
 			ctx.lineTo(begin,570)
 		}
 		ctx.stroke()
 		ctx.closePath()
+		//draw point
+		drawPiece(119,118,'gray',6)
+		drawPiece(479,478,'gray',6)
+		drawPiece(119,478,'gray',6)
+		drawPiece(479,118,'gray',6)
+		drawPiece(299,298,'gray',6)
+
+		drawPiece(299,118,'gray',6)
+		drawPiece(299,478,'gray',6)
+		drawPiece(119,298,'gray',6)
+		drawPiece(479,298,'gray',6)
 	}
 
 	/*draw pieces
 	*/
-	function drawPiece(posx,posy,color) {
+	function drawPiece(posx,posy,color,r) {
 		ctx.fillStyle=color
 		ctx.beginPath()
-		ctx.arc(posx,posy,RADIUS,0,Math.PI*2)
+		ctx.arc(posx,posy,r,0,Math.PI*2)
 		ctx.fill()
 		ctx.closePath()
 	}
-	var color='black'
+
+	/*
+	*function:judge the location has pieces
+	*/
+	function hadPiece(x,y) {
+		var had=false
+		for(var i=0,len=blackPieces.length;i<len;i++){
+			if (x==blackPieces[i].pieceX&&y==blackPieces[i].pieceY) {
+				had=true
+			}
+		}
+		for(i=0,len=whitePieces.length;i<len;i++){
+			if (x==whitePieces[i].pieceX&&y==whitePieces[i].pieceY) {
+				had=true
+				// console.log("param:"+x,y)
+				// console.log("array:"+whitePieces.pieceX,whitePieces[i].pieceY)
+			}
+		}
+		return had
+	}
+
+
+	/*
+	*judge has win
+	*/
+	function hasWin(arr) {
+		var win=false
+		if(arr.length>4){
+			//sort the arr
+			arr.sort(function (cur,next) {
+				if (cur.piecesX<next.pieceX) {
+					return 0
+				}
+				else if(cur.pieceX==next.piecesX){
+					if (cur.pieceY<next.pieceY) {
+						return 0
+					}
+					else  {
+						return 1
+					}
+				}
+				else if (cur.pieceX>next.pieceX) {
+					return 1
+				}
+			})
+		}
+		return false
+	}
+		
+	/*
+	*function sort array
+	*/
+	function sortPieces(arr){
+	//sort the arr
+		arr.sort(function (cur,next) {
+			if (cur.piecesX<next.pieceX) {
+				return 0
+			}
+			else if(cur.pieceX==next.piecesX){
+				if (cur.pieceY<next.pieceY) {
+					return 0
+				}
+				else  {
+					return 1
+				}
+			}
+			else if (cur.pieceX>next.pieceX) {
+				return 1
+			}
+		})
+	}
+
 	//add event listener
 	cav.addEventListener('click', function (e) {
-		console.log(e.clientX-cavLeft,e.clientY-cavTop)
-		var x=Math.round((e.clientX-cavLeft-28)/INTERVAL)*INTERVAL+26
-		var y=Math.round((e.clientY-cavTop-28)/INTERVAL)*INTERVAL+26
 		
-		drawPiece(x,y,color)
-		if (color=='black') {
-			color='white'
-		}else{
-			color='black'
+		var x=Math.round((e.clientX-cavLeft-28)/INTERVAL)
+		var y=Math.round((e.clientY-cavTop-28)/INTERVAL)
+		console.log(x,y)
+		var had=hadPiece(x,y)
+		if(!had){
+			drawPiece(x*INTERVAL+29,y*INTERVAL+28,color,RADIUS)
+			var singlePiece={
+				pieceX:x,
+				pieceY:y
+			}
+			if (color=='black') {
+				color='white'
+				blackPieces.push(singlePiece)
+				sortPieces(blackPieces)
+				console.log(blackPieces)
+			}else{
+				color='black'
+				whitePieces.push(singlePiece)
+				sortPieces(whitePieces)
+			}
+			
 		}
+		
+	}, false)//end event listener
 
-
-	}, false)
-
+	
 	drawBoard()
 
 
@@ -87,11 +187,11 @@
 	// drawPiece(28,28+INTERVAL,'red')
 	// drawPiece(28+INTERVAL*20,28+INTERVAL*20,'red')
 	function drawAll() {
-		for(var i=0;i<21;i++){
-			var x=26+i*INTERVAL
-			for(var j=0;j<21;j++){
-				var y=26+j*INTERVAL
-				drawPiece(x,y,'black')
+		for(var i=0;i<19;i++){
+			var x=29+i*INTERVAL
+			for(var j=0;j<19;j++){
+				var y=28+j*INTERVAL
+				drawPiece(x,y,'black',RADIUS)
 			}
 		}
 	}
